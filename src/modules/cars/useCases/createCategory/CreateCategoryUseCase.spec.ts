15d873b3056490bd03b56d26a4dac55c1bfd589a
Armando Marioto <armando_marioto100@hotmail.com>
2022-12-11 11:22:26 -0300

    Criando o primeiro teste

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Criar categoria", () => {
+  it("Espero que ...", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+
+  it("Espero que  não...", () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).not.toBe(resultado);
+  });
+});
